9f57d6344c03d4f88e251b99899bef7223b29610
ABHAY VERMA <144254558+Abhay-Verma-2005@users.noreply.github.com>
2026-01-19 14:58:56 +0530

    Time: 205 ms (6.70%) | Memory: 47 MB (5.06%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,16 +1,19 @@
 class Solution {
     public boolean isValid(String s) {
         while(true){
-            if(s.contains("[]")) s=s.replace("[]","");
-            else if(s.contains("()")) s=s.replace("()","");
-            else if(s.contains("{}")) s=s.replace("{}","");
-            else if(!s.contains("()") && !s.contains("[]") && !s.contains("{}")){
-                break;
+            if(s.contains("()") || s.contains("[]") || s.contains("{}")){
+                s=s.replace("[]","");
+                s=s.replace("()","");
+                s=s.replace("{}","");
             }
-            if(s.equals("")){
+            if(s.length()==0){
                 return true;
             }
-       }
+            if(!s.contains("()") && !s.contains("[]") && !s.contains("{}")){
+                break;
+            }
+
+        }
         return false;
     }
 }
