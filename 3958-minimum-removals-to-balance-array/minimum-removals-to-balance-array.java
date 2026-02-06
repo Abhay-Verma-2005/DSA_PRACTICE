5e96f7eb502990190ec72546623e1cb2c168e8ee
ABHAY VERMA <144254558+Abhay-Verma-2005@users.noreply.github.com>
2026-02-06 23:44:32 +0530

    Time: 23 ms (100.00%) | Memory: 86.3 MB (19.68%) - LeetSync

---
+++ 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int minRemoval(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int left = 0;
+        int maxWindow = 0;
+
+        for (int right = 0; right < n; right++) {
+            if ((long) nums[right] > (long) nums[left] * k) {
+                left++;
+            }
+            maxWindow = Math.max(maxWindow, right - left + 1);
+        }
+
+        return n - maxWindow;
+    }
+}
