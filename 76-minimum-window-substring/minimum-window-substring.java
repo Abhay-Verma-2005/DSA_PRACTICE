a9b24310b1a242ba45d66580b89d92a62d087bba
ABHAY VERMA <144254558+Abhay-Verma-2005@users.noreply.github.com>
2026-03-11 12:30:38 +0530

    Time: 31 ms (19.37%) | Memory: 45.4 MB (89.93%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -2,36 +2,29 @@ class Solution {
     public String minWindow(String s, String t) {
 
         int[] freq = new int[128];
-
         for(char c : t.toCharArray()){
             freq[c]++;
         }
-
         int l = 0;
-        int minLen = Integer.MAX_VALUE;
-        int start = 0;
-
+        int Len = Integer.MAX_VALUE;
+        int st = 0;
         for(int r = 0; r < s.length(); r++){
-
             char c = s.charAt(r);
             freq[c]--;
-
             while(check(freq)){
-
-                if(r - l + 1 < minLen){
-                    minLen = r - l + 1;
-                    start = l;
+                if(r-l+1<Len){
+                    Len=r-l+1;
+                    st=l;
                 }
-
-                char left = s.charAt(l);
+                char left=s.charAt(l);
                 freq[left]++;
                 l++;
             }
         }
 
-        if(minLen == Integer.MAX_VALUE) return "";
+        if(Len == Integer.MAX_VALUE) return "";
 
-        return s.substring(start, start + minLen);
+        return s.substring(st, st + Len);
     }
 
     public boolean check(int[] freq){
