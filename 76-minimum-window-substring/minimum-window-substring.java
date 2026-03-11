81ffc037d235d85bba2792e056ee6bd53b5fec07
ABHAY VERMA <144254558+Abhay-Verma-2005@users.noreply.github.com>
2026-03-11 12:23:52 +0530

    Time: 231 ms (5.49%) | Memory: 47.2 MB (50.53%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -4,39 +4,35 @@ class Solution {
         int m=t.length();
         int c=Integer.MAX_VALUE;
         StringBuilder ans=new StringBuilder("");
-        HashMap<Character,Integer> Smap=new HashMap<>();
-        HashMap<Character,Integer> Tmap=new HashMap<>();
+        int[] freq=new int[128];
         for(char ch:t.toCharArray()){
-            Tmap.put(ch,Tmap.getOrDefault(ch,0)+1);
+            freq[ch]++;
         }
         int l=0;
         for(int i=0;i<n;i++){
             char ch=s.charAt(i);
             //grow
-            Smap.put(ch,Smap.getOrDefault(ch,0)+1);
+            freq[(int) ch]--;
             if(i-l+1>=m){
-                while(check(Smap,Tmap)){
-
+                while(check(freq)){
                     if(i-l+1<c){
                         c = i-l+1;
                         ans = new StringBuilder(s.substring(l,i+1));
                     }
-
                     char chr = s.charAt(l);
-                    Smap.put(chr, Smap.get(chr)-1);
+                    if(t.contains(chr+"")){
+                        freq[(int)chr]++;
+                    }
                     l++;
                 }
             }
         }
-            return ans.toString();
+        return ans.toString();
 
     }
-    public boolean check(HashMap<Character,Integer> Smap ,HashMap<Character,Integer> Tmap){
-        for(char ele:Tmap.keySet()){
-            if(!Smap.containsKey(ele) || Smap.get(ele) < Tmap.get(ele)){
-                return false;
-            }
-
+    public boolean check(int[] freq){
+        for(int ele:freq){
+            if(ele>0) return false;
         }
         return true;
     }
