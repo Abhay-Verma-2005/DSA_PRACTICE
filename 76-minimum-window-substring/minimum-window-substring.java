19d0143bb0559126ecfc540dc5628c23f276559e
ABHAY VERMA <144254558+Abhay-Verma-2005@users.noreply.github.com>
2026-03-11 12:28:47 +0530

    Time: 32 ms (19.15%) | Memory: 45.7 MB (73.08%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,38 +1,42 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int n=s.length();
-        int m=t.length();
-        int c=Integer.MAX_VALUE;
-        StringBuilder ans=new StringBuilder("");
-        int[] freq=new int[128];
-        for(char ch:t.toCharArray()){
-            freq[ch]++;
+
+        int[] freq = new int[128];
+
+        for(char c : t.toCharArray()){
+            freq[c]++;
         }
-        int l=0;
-        for(int i=0;i<n;i++){
-            char ch=s.charAt(i);
-            //grow
-            freq[(int) ch]--;
-            if(i-l+1>=m){
-                while(check(freq)){
-                    if(i-l+1<c){
-                        c = i-l+1;
-                        ans = new StringBuilder(s.substring(l,i+1));
-                    }
-                    char chr = s.charAt(l);
-                    if(t.contains(chr+"")){
-                        freq[(int)chr]++;
-                    }
-                    l++;
+
+        int l = 0;
+        int minLen = Integer.MAX_VALUE;
+        int start = 0;
+
+        for(int r = 0; r < s.length(); r++){
+
+            char c = s.charAt(r);
+            freq[c]--;
+
+            while(check(freq)){
+
+                if(r - l + 1 < minLen){
+                    minLen = r - l + 1;
+                    start = l;
                 }
+
+                char left = s.charAt(l);
+                freq[left]++;
+                l++;
             }
         }
-        return ans.toString();
 
+        if(minLen == Integer.MAX_VALUE) return "";
+
+        return s.substring(start, start + minLen);
     }
+
     public boolean check(int[] freq){
-        for(int ele:freq){
-            if(ele>0) return false;
+        for(int f : freq){
+            if(f > 0) return false;
         }
         return true;
     }
