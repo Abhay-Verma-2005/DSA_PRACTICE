162c4d1e18647d44be21379860c219f168da6fc2
ABHAY VERMA <144254558+Abhay-Verma-2005@users.noreply.github.com>
2026-03-17 12:02:15 +0530

    Time: 23 ms (16.45%) | Memory: 50.7 MB (12.75%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,13 +1,12 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
         ArrayList<HashMap<Integer,Integer>> g= new ArrayList<>();
-        for(int i=0;i<n;i++){
+        for(int i=0;i<=n;i++){
             g.add(new HashMap<Integer,Integer>());
         }
         for(int [] ele: times){
-            g.get(ele[0]-1).put(ele[1]-1,ele[2]);
+            g.get(ele[0]).put(ele[1],ele[2]);
         }
-        k--;
         PriorityQueue<Pair> pq= new PriorityQueue<>((a,b)->a.cost-b.cost);
         pq.add(new Pair(k,0));
         int ans=-1;
