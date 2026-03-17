f2d45509e727d0ceef230d90941b1d88a4d15c81
ABHAY VERMA <144254558+Abhay-Verma-2005@users.noreply.github.com>
2026-03-17 11:30:55 +0530

    Time: 885 ms (8.05%) | Memory: 273.1 MB (5.00%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -1,43 +1,62 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-        ArrayList<HashMap<Integer,Integer>> g= new ArrayList<>();
-        int n=points.length;
-        for(int i=0;i<n;i++){
-            g.add(i,new HashMap<>());
-        }
-        for(int i=0;i<n;i++){
-            for(int j=1;j<n;j++){
-                g.get(i).put(j,Math.abs(points[j][0]-points[i][0])+Math.abs(points[j][1]-points[i][1]));
-                g.get(j).put(i,Math.abs(points[j][0]-points[i][0])+Math.abs(points[j][1]-points[i][1]));
-            }
-        }
-        PriorityQueue<Pair> pq= new PriorityQueue<>((a,b)->a.cost-b.cost);
-        HashSet<Integer> vis= new HashSet<>();
-        pq.add(new Pair(0,0,0));
-        int sum=0;
-        while(!pq.isEmpty()){
-            Pair rm= pq.poll();
-            int v=rm.vtx;
-            int a=rm.acq;
-            int c=rm.cost;
-            if(vis.contains(v)) continue;
-            vis.add(v);
-            sum+=c;
-            for(int ng: g.get(v).keySet()){
-                    if(!vis.contains(ng)){
-                        int cost=g.get(v).get(ng);
-                        pq.add(new Pair(ng,v,cost));
-                    }
-            }
 
+        ArrayList<HashMap<Integer,Integer>> g = new ArrayList<>();
+        int n = points.length;
+
+        for(int i=0;i<n;i++){
+            g.add(new HashMap<>());
         }
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+
+                int cost = Math.abs(points[j][0]-points[i][0]) +
+                           Math.abs(points[j][1]-points[i][1]);
+
+                g.get(i).put(j,cost);
+                g.get(j).put(i,cost);
+            }
+        }
+
+        PriorityQueue<Pair> pq =
+            new PriorityQueue<>((a,b)->a.cost-b.cost);
+
+        HashSet<Integer> vis = new HashSet<>();
+
+        pq.add(new Pair(0,0,0));
+
+        int sum=0;
+
+        while(!pq.isEmpty()){
+
+            Pair rm = pq.poll();
+            int v = rm.vtx;
+            int c = rm.cost;
+
+            if(vis.contains(v)) continue;
+
+            vis.add(v);
+            sum += c;
+
+            for(int ng : g.get(v).keySet()){
+                if(!vis.contains(ng)){
+                    int cost = g.get(v).get(ng);
+                    pq.add(new Pair(ng,v,cost));
+                }
+            }
+        }
+
         return sum;
     }
+
     public class Pair{
+
         int vtx;
         int acq;
         int cost;
-        public Pair(int a, int b, int c){
+
+        public Pair(int a,int b,int c){
             vtx=a;
             acq=b;
             cost=c;
