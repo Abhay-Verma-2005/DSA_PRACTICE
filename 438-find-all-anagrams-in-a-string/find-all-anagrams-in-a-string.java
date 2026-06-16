bda188782406f41cd0cb8f3eb7453466f101eac3
ABHAY VERMA <144254558+Abhay-Verma-2005@users.noreply.github.com>
2026-06-16 22:09:52 +0530

    Time: 11 ms (66.46%) | Memory: 46.9 MB (45.68%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,20 +1,35 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> ans = new ArrayList<>();
-        int k = p.length();
-        int[] pCount = new int[26];
-        for (char ch : p.toCharArray()) pCount[ch - 'a']++;
-        
-        for (int i = 0; i <= s.length() - k; i++) {
-            int[] subCount = new int[26];
-            for (int j = i; j < i + k; j++) {
-                subCount[s.charAt(j) - 'a']++;
-            }
-            if (Arrays.equals(subCount, pCount)) {
-                ans.add(i);
-            }
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        if (s.length() < p.length()) return ans;
+
+        int[] arr = new int[26];
+
+        for (char ch : p.toCharArray()) {
+            arr[ch - 'a']++;
         }
-        
+
+        for (int i = 0; i < p.length(); i++) {
+            arr[s.charAt(i) - 'a']--;
+        }
+
+        for (int i = p.length(); i < s.length(); i++) {
+            if (check(arr)) ans.add(i - p.length());
+
+            arr[s.charAt(i - p.length()) - 'a']++;
+            arr[s.charAt(i) - 'a']--;
+        }
+
+        if (check(arr)) ans.add(s.length() - p.length());
+
         return ans;
+    }
+
+    public boolean check(int[] arr) {
+        for (int e : arr) {
+            if (e != 0) return false;
+        }
+        return true;
     }
 }
