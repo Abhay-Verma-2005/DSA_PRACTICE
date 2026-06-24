7f8cd9124abba511ef072d93ac1fde699ad3a432
ABHAY VERMA <144254558+Abhay-Verma-2005@users.noreply.github.com>
2026-06-24 10:52:31 +0530

    Time: 1168 ms (8.86%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,14 +1,42 @@
-SELECT d.name as Department, e.name AS Employee,e.salary AS Salary
-FROM Employee e 
-JOIN Department d 
-On e.departmentId=d.id
-JOIN ( SELECT departmentId,MAX(salary) AS maxSalary
+-- SELECT d.name as Department, e.name AS Employee,e.salary AS Salary
+-- FROM Employee e 
+-- JOIN Department d 
+-- On e.departmentId=d.id
+-- JOIN (SELECT departmentId,MAX(salary) AS maxSalary
+--     FROM Employee
+--     GROUP BY departmentId
+-- ) t
+-- ON e.departmentId = t.departmentId
+-- where e.salary = t.maxSalary;
+
+
+
+
+-- SELECT D.name AS Department,E.name AS Employee,E.salary AS Salary
+-- FROM Employee E
+-- JOIN 
+--     Department D 
+--     ON E.departmentId = D.id
+-- WHERE 
+--     (E.departmentId, E.salary) IN (
+--         SELECT 
+--             departmentId, 
+--             MAX(salary)
+--         FROM 
+--             Employee
+--         GROUP BY 
+--             departmentId
+--     );
+
+
+
+SELECT d.name AS Department, 
+       e.name AS Employee, 
+       e.salary AS Salary
+FROM Employee e
+JOIN Department d ON e.departmentId = d.id
+WHERE e.salary = (
+    SELECT MAX(salary)
     FROM Employee
-    GROUP BY departmentId
-) t
-ON e.departmentId = t.departmentId
-where e.salary = t.maxSalary;
-
-
-
-
+    WHERE departmentId = e.departmentId
+);
