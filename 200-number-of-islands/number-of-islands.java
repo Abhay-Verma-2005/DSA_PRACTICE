9687051dc53b926571d5cccb57512ed1c4584fdd
ABHAY VERMA <144254558+Abhay-Verma-2005@users.noreply.github.com>
2026-06-25 10:50:44 +0530

    Time: 3 ms (87.63%) | Memory: 52.1 MB (72.74%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -3,25 +3,23 @@ class Solution {
         int n=grid.length;
         int m=grid[0].length;
         int ans=0;
-        boolean[][] vis= new boolean[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(!vis[i][j] && grid[i][j]=='1'){
+                if(grid[i][j]=='1'){
                     ans++;
-                    mark(grid, i, j, vis);
+                    mark(grid, i, j);
                 }
             }
         }
         return ans;
     }
-    public void mark(char[][] grid,int i , int j, boolean[][] vis){
+    public void mark(char[][] grid,int i , int j){
         if(i<0 || j<0 || i>=grid.length || j>=grid[0].length) return;
         if(grid[i][j]!='1') return;
-        if(vis[i][j]) return;
-        vis[i][j]=true;
-        mark(grid,i+1,j,vis);
-        mark(grid,i-1,j,vis);
-        mark(grid,i,j-1,vis);
-        mark(grid,i,j+1,vis);
+        grid[i][j]='0';
+        mark(grid,i+1,j);
+        mark(grid,i-1,j);
+        mark(grid,i,j-1);
+        mark(grid,i,j+1);
     }
 }
