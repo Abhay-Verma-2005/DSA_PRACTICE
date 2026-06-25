42855ae988686b78b0000e32ecaea1facf7a9203
ABHAY VERMA <144254558+Abhay-Verma-2005@users.noreply.github.com>
2026-06-25 09:17:08 +0530

    Time: 1021 ms (5.05%) | Memory: 46.4 MB (49.12%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,24 +1,23 @@
 class Solution {
     int ans=Integer.MAX_VALUE;
     public int coinChange(int[] coins, int amount) {
-        int[] dp= new int[amount+1];
+        int[] dp = new int[amount+1];
         Arrays.fill(dp,-1);
         solve(coins,amount,0,dp);
-        if(ans==Integer.MAX_VALUE) ans=-1;
+        if(ans==Integer.MAX_VALUE) return -1;
         return ans;
     }
-    public void solve(int[] coins, int amt , int curr, int[]dp){
+    public void solve(int[] coins, int amt, int cnt,int[] dp ){
         if(amt==0){
-            ans=Math.min(ans,curr);
+            ans=Math.min(ans,cnt);
             return;
         }
-        if(curr>=ans) return;
-        if(dp[amt]!=-1 && dp[amt]<=curr) return;
-        dp[amt]=curr;
-        for(int i:coins) {
-            if(amt >= i){
-                solve(coins, amt-i,curr+1,dp);
-            }
+        if(amt<0) return;
+        if(dp[amt]!=-1 && dp[amt]<=cnt) return;
+        dp[amt]=cnt;
+        if(dp[amt]!=-1)
+        for(int ele: coins){
+            solve(coins, amt-ele,cnt+1,dp);
         }
     }
 }
